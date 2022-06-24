ea42b9cf25ce3f1bd8d8bb55cb7d20928610b939
vrob13 <robinsonvegas@gmail.com>
2022-06-23 23:20:37 -0400

    listo para deploy

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from "react";
+import reactDom from "react-dom";
+import App from './components/App';
+import './styles/global.scss';
+
+reactDom.render(<App />, document.getElementById('app'));
---
+++ webpack.config.js
@@ -0,0 +1,106 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MIniCssExtractPlugin = require('mini-css-extract-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
+const { clearWebpackPlugin } = require('clear-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: "/",
+  },
+  resolve: {
+    extensions: ['.js', '.jsx'],
+    alias: {
+      '@components': path.resolve(__dirname, 'src/components/'),
+      '@styles': path.resolve(__dirname, 'src/styles/')
+    }
+  },
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        }
+      },
+      {
+        test: /\.html$/,
+        use: [
+          { loader: 'html-loader' }
+        ]
+      },
+      {
+        test: /\.s[ac]ss$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          'sass-loader'
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './public/index.html',
+      filename: './index.html'
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css'
+    }),
+    new clearWebpackPlugin(),
+  ],
+  optimization: {
+    minimizer: true,
+    minimizer: [
+      new CssMinimizerPlugin(),
+      new TerserPlugin(),
+    ]
+  }
+}
+// //const { resolve } = require('path');
+// const path = require('path');
+// const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+// module.exports = {
+//     entry: './src/index.js',
+//     output: {
+//       path: path.resolve(__dirname, 'dist'),
+//       filename: 'bundle.js',
+//     },
+//     resolve: {
+//       extensions: ['.js', '.jsx']
+//     },
+//     module: {
+//       rules: [
+//         {
+//           test: /\.(js|jsx)$/,
+//           exclude: /node_modules/,
+//           use: {
+//             louder: 'babel-loader',
+//           }
+//         },
+//         {
+//           test: /\.html$/,
+//           use: [{ loader: 'html-loader' }],
+//         },
+//       ],
+//     },
+//     Plugins: [
+//       new HtmlWebpackPlugin({
+//         template: './public/index.html',
+//         filename: './index.html'
+//       })
+//     ],
+//   devServer: {
+//         static: path.join(__dirname, 'dist'),
+//         compress: true,
+//         port: 3006
+//     }
+// }
